f76497d16188384979ed340dc5d3c78258d7c700
Sunny <swarnendughosh05@gmail.com>
2024-11-08 16:13:21 +0530

    dashboard modifyed

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,59 +1,74 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
-import React from 'react'
-import { HOOKS_URL } from '../config';
+import { NavBar } from '@/components/NavBar';
 import { Button } from '@/components/ui/button';
-import { useZaps, Zap } from '../../../hooks/userzap';
-import Navbar from '@/components/navbar';
-
-
+import { useZaps, Zap } from '@/hooks/userZap';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { HOOKS_URL } from '../config';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import Image from 'next/image';
 
 export default function Dashboard() {
-  const routet = useRouter()
-  const { loading, zaps} = useZaps();
+  const routet = useRouter();
+  const { loading, zaps } = useZaps();
+
   return (
     <div>
-        <Navbar />
-        <div className='pt-8 flex justify-center'>
-          <div className='max-w-screen-lg w-full'>
-           <div className='flex justify-between pr-8'>
-              <div className='font-bold text-2xl'>
-                My Zaps
-              </div>
-            <Button className='bg-purple-800' onClick={() => {
-               routet.push("/zap/create")
-            }}>
+      <NavBar />
+      <div className='pt-8 flex justify-center'>
+        <div className='max-w-screen-lg w-full'>
+          <div className='flex justify-between pr-8'>
+            <div className='font-bold text-2xl'>My Zaps</div>
+            <Button className='bg-purple-800' onClick={() => routet.push("/zap/create")}>
               Create
             </Button>
-             </div> 
-           </div>
+          </div>
         </div>
-        {loading ? "Loading": <ZapTable zaps={zaps}/>}
+      </div>
+      {loading ? "Loading" : <ZapTable zaps={zaps} />}
     </div>
-  )
+  );
 }
 
-function ZapTable({ zaps }: {zaps: Zap[]}) { 
+function ZapTable({ zaps }: { zaps: Zap[] }) {
   const router = useRouter();
 
-  return <div className="p-8 max-w-screen-lg w-full bg-red-600 ">
-      <div className="flex justify-center space-x-16 bg-cyan-700">
-              <div className="flex-1">Name</div>
-              <div className="flex-1">ID</div>
-              <div className="flex-1">Created at</div>
-              <div className="flex-1">Webhook URL</div>
-              <div className="flex-1">Go</div>
-      </div>
-      {zaps.map(z => <div className="flex border-b border-t py-4 space-x-16">
-          <div className="flex-1 flex"><img src={z.trigger?.type?.image} className="w-[30px] h-[30px]" /> 
-          {z.actions.map((x, idx) => (<img key={idx} src={x.type.image} className="w-[30px] h-[30px]" />))}</div>
-          <div className="flex-1">{z.id}</div>
-          <div className="flex-1">Nov 13, 2023</div>
-          <div className="flex-1">{`${HOOKS_URL}/hooks/catch/1/${z.id}`}</div>
-          <div className="flex-1"><Button onClick={() => {
-                  router.push("/zap/" + z.id)
-              }}>Go</Button></div>
-      </div>)}
-  </div>
+  return (
+    <div className="p-8 max-w-screen-lg w-full">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>ID</TableHead>
+            <TableHead>Created At</TableHead>
+            <TableHead>Webhook URL</TableHead>
+            <TableHead>Go</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {zaps.map((z) => (
+            <TableRow key={z.id}>
+              <TableCell>
+                <div className="flex">
+                  {z.trigger?.type?.image && (
+                    <img src={z.trigger.type.image} alt="Webhook" width={30} height={30} />
+                  )}
+                  {z.actions.map((action, idx) => (
+                    <img key={idx} src={action.type.image} alt="Action" width={30} height={30} />
+                  ))}
+                </div>
+              </TableCell>
+              <TableCell>{z.id}</TableCell>
+              <TableCell>Nov 13, 2023</TableCell>
+              <TableCell>{`${HOOKS_URL}/hooks/catch/1/${z.id}`}</TableCell>
+              <TableCell>
+                <Button onClick={() => router.push("/zap/" + z.id)}>Go</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
