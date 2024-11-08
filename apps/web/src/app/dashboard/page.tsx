c782f2fc34659f0a1a75cea41078caf515d2c79d
Sunny <swarnendughosh05@gmail.com>
2024-11-08 12:22:26 +0530

    starting script added

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -37,8 +37,8 @@ export default function Dashboard() {
 function ZapTable({ zaps }: {zaps: Zap[]}) { 
   const router = useRouter();
 
-  return <div className="p-8 max-w-screen-lg w-full">
-      <div className="flex justify-center space-x-16">
+  return <div className="p-8 max-w-screen-lg w-full bg-red-600 ">
+      <div className="flex justify-center space-x-16 bg-cyan-700">
               <div className="flex-1">Name</div>
               <div className="flex-1">ID</div>
               <div className="flex-1">Created at</div>
@@ -47,7 +47,7 @@ function ZapTable({ zaps }: {zaps: Zap[]}) {
       </div>
       {zaps.map(z => <div className="flex border-b border-t py-4 space-x-16">
           <div className="flex-1 flex"><img src={z.trigger?.type?.image} className="w-[30px] h-[30px]" /> 
-          {z.actions.map((x, idx) => ( <img key={idx} src={x.type.image} className="w-[30px] h-[30px]" />)) }</div>
+          {z.actions.map((x, idx) => (<img key={idx} src={x.type.image} className="w-[30px] h-[30px]" />))}</div>
           <div className="flex-1">{z.id}</div>
           <div className="flex-1">Nov 13, 2023</div>
           <div className="flex-1">{`${HOOKS_URL}/hooks/catch/1/${z.id}`}</div>
