b0cfa4e633cad2ece18f6503cb4aa0e7d1dd10ad
Sunny <swarnendughosh05@gmail.com>
2024-11-10 18:13:33 +0530

    casweing isssu fixedd in frontend

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,13 @@
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
+  webpack: (config) => {
+    // Add CaseSensitivePathsPlugin to the Webpack plugins
+    config.plugins.push(new CaseSensitivePathsPlugin());
+    
+    return config;
+  },
 };
